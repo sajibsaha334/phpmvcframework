9dd72ca1a8d66525e5edfce24a26c196420ca77e
Sajib Saha <sajib.saha@brainstation-23.com>
2025-04-08 17:01:26 +0600

    Initial Commit

---
+++ controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Application;
+use app\core\Controller;
+
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return $this->render('login');
+    }
+
+    public function register()
+    {
+        return $this->render('register');
+    }
+}
